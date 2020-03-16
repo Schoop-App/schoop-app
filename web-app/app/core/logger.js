36cfc371c22001d5b533e0f38fe07d2bc3806949
zane-programs <zstjohn22@windwardschool.org>
2020-03-16 00:13:24 -0700

    fix logging temp

--- web-app/app/core/logger.js
+++ web-app/app/core/logger.js
@@ -2,7 +2,8 @@
 // would suit real logging, but I'm a bit pressed
 // for time. Let's hope this does the trick.
 
-let shouldLog = process.env.NODE_ENV !== "production";
+// let shouldLog = process.env.NODE_ENV !== "production";
+let shouldLog = true;
 
 function log() {
 	if (shouldLog) console.log(...arguments);
