772b08699336e979c8027a3658100006a3ee9810
zane-programs <zstjohn22@windwardschool.org>
2020-03-18 15:28:33 -0700

    testing register student functionality

--- web-app/app/routes/auth.js
+++ web-app/app/routes/auth.js
@@ -15,7 +15,24 @@ module.exports = imports => {
 		async (req, res) => {
 			logger.log("user authenticated");
 			let studentIsRegistered = await db.doesStudentExist(req.user.id);
-			res.status(200).send({ studentIsRegistered });
+			if (studentIsRegistered) {
+				res.status(200).send("You are already registered with us.");
+			} else {
+				// studentId, firstName, lastName, email, profilePicUrl
+				 // returns boolean
+				let studentRegistryQuery = await db.addStudent(req.user.id,
+															 	req.user.name.givenName,
+															 	req.user.name.familyName,
+															 	req.user.emails[0].value,
+															 	req.user.photos[0].value);
+
+				if (studentRegistryQuery) {
+					// successfully registered
+					res.status(200).send("Successfully registered!");
+				} else {
+					res.status(500).send("We were not able to register you. Please try again.");
+				}
+			}
 
 			// res.status(200).send(req.user);
 		}
