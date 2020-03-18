345a0d881c78f5c065de99a1b0ba432b665b0ad9
Zane St. John <zstjohn22@windwardschool.org>
2020-03-18 22:11:14 +0000

    fixes to auth

--- web-app/app/routes/auth.js
+++ web-app/app/routes/auth.js
@@ -14,8 +14,7 @@ module.exports = imports => {
 		passport.authenticate("google", { failureRedirect: "/?failed=1", session: true }),
 		async (req, res) => {
 			logger.log("user authenticated");
-			let studentIsRegistered = await db.doesStudentExist(user.id);
-
+			let studentIsRegistered = await db.doesStudentExist(req.user.id);
 			res.status(200).send({ studentIsRegistered });
 
 			// res.status(200).send(req.user);
