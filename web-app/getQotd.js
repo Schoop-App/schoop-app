875d4f7a9ab4434decf18d9ff0e557b27161d4cc
zane-programs <zstjohn22@windwardschool.org>
2020-03-21 11:46:28 -0700

    created qotd thing to get quote of the day

---
+++ web-app/getQotd.js
@@ -0,0 +1,14 @@
+const axios = require("axios");
+const redisClient = require("redis").createClient();
+const setAsync = require("util").promisify(redisClient.set).bind(redisClient);
+
+(async () => {
+	try {
+		let qotdReq = await axios.get("https://api.quotable.io/random?maxLength=50&tags=inspirational");
+		await setAsync("schoop:qotd", JSON.stringify(qotdReq.data));
+		process.exit(0);
+	} catch (e) {
+		console.log("Error getting quote:");
+		console.error(e);
+	}
+})();
