957131590f218ae3cde1036afd7ae009fa91b2c9
zane-programs <zstjohn22@windwardschool.org>
2020-03-24 18:43:36 -0700

    created db client test to debug issue with adding classes (none of them got added lol)

---
+++ web-app/db-test.js
@@ -0,0 +1,33 @@
+const mysql = require("mysql");
+
+const PRIVATE_CONFIG = require("./private-config.json"); // private info
+
+const dbConn = mysql.createConnection({
+	host: PRIVATE_CONFIG.database.host,
+	user: PRIVATE_CONFIG.database.user,
+	password: PRIVATE_CONFIG.database.password,
+	database: PRIVATE_CONFIG.database.database,
+	port: PRIVATE_CONFIG.database.port,
+	ssl: {
+		ca: fs.readFileSync(process.env.DATABASE_CERT_PATH)
+	}
+});
+
+console.log("Connecting to database...");
+dbConn.connect(async err => {
+	if (err) {
+		console.error(err);
+	}
+	console.log("Connected to database");
+
+	// db client
+	const db = require("./app/core/db")({
+		Sentry: {
+			captureException: e => console.error(e)
+		},
+		dbConn
+	});
+
+	let classQuery = await db.addClass("101538478513395768684", 4, "PreCalc Honors", "https://windwardschool.zoom.us/j/4242891086");
+	console.log(classQuery);
+});
