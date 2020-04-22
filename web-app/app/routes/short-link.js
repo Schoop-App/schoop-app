ac53978b859b1a47d194756b7178200f9c391021
zane-programs <zstjohn22@windwardschool.org>
2020-04-22 10:05:25 -0700

    fix??

--- web-app/app/routes/short-link.js
+++ web-app/app/routes/short-link.js
@@ -6,6 +6,14 @@ module.exports = imports => {
 
 	const router = require("express").Router();
 
+	router.get("/event_redirect", (req, res) => {
+		if (req.isAuthenticated()) {
+			res.redirect(req.query.url);
+		} else {
+			res.status(200).send(`Log in to access this page.<br><a href="/login?redirect=${encodeURIComponent(req.originalUrl)}">Click here to log in and access the link.</a>`);
+		}
+	});
+
 	router.get("/:classId", async (req, res) => {
 		if (req.isAuthenticated()) {
 			let classLink = await db.getClassLinkForStudent(req.params.classId, req.user.id);
@@ -13,14 +21,6 @@ module.exports = imports => {
 				res.status(404).send("Error - Not Found");
 			else
 				res.redirect(classLink);
-		} else {
-			res.status(200).send(`Log in to access this page.<br><a href="/login?redirect=${encodeURIComponent(req.originalUrl)}">Click here to log in and access the link.</a>`);
-		}
-	});
-
-	router.get("/event_redirect", (req, res) => {
-		if (req.isAuthenticated()) {
-			res.redirect(req.query.url);
 		} else {
 			res.status(200).send(`Log in to access this page.<br><a href="/login?redirect=${encodeURIComponent(req.originalUrl)}">Click here to log in and access the link.</a>`);
 		}
