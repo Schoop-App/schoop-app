1541311525dbcd6d4b86f2fa917696469aa6d0cb
zane-programs <zstjohn22@windwardschool.org>
2020-05-16 16:05:19 -0700

    fixed exec-send for prod

---
+++ web-app/send-emails/core/send-util.js
@@ -0,0 +1,24 @@
+const sendEmails = async (db, studentIds) => {
+	try {
+		let numStudentIds = studentIds.length;
+		let currentStudentId;
+		for (let i = 1; i <= numStudentIds; i++) {
+			currentStudentId = studentIds[i - 1];
+			// sending email message
+			console.log(`Sending email ${i} / ${numStudentIds} (${getPercentage(i - 1, numStudentIds)} done) to ${currentStudentId}`);
+
+			// actually sending the email via client
+			await emailClient.sendScheduleEmail(db, currentStudentId);
+
+			// email sent messgae
+			console.log(`Email sent to ${currentStudentId}\n`);
+		}
+	} catch (e) {
+		console.log("Error sending emails:");
+		console.error(e);
+	}
+};
+
+module.exports = {
+	sendEmails
+};
--- web-app/send-emails/exec-send.js
+++ web-app/send-emails/exec-send.js
@@ -21,40 +21,18 @@ const dbConn = mysql.createConnection({
 
 const getPercentage = (index, outOf) => (index / outOf * 100).toPrecision(3) + "%";
 
-const sendEmails = async (db, studentIds) => {
-	try {
-		let numStudentIds = studentIds.length;
-		let currentStudentId;
-		for (let i = 1; i <= numStudentIds; i++) {
-			currentStudentId = studentIds[i - 1];
-			// sending email message
-			console.log(`Sending email ${i} / ${numStudentIds} (${getPercentage(i - 1, numStudentIds)} done) to ${currentStudentId}`);
-
-			// actually sending the email via client
-			await emailClient.sendScheduleEmail(db, currentStudentId);
-
-			// email sent messgae
-			console.log(`Email sent to ${currentStudentId}\n`);
-		}
-	} catch (e) {
-		console.log("Error sending emails:");
-		console.error(e);
-	}
-};
-
-
 // begin actual "meat" of the program
 
 let date = new Date();
-if (date.getDay() !== 6 && date.getDay() !== 0) {
+// if (date.getDay() !== 6 && date.getDay() !== 0) {
+if (true) {
 	// this is just a fail-safe. the cron job knows to send monday thru friday only
 	console.log("Connecting...");
 	dbConn.connect(async err => {
 		const db = require("../app/core/db")({ Sentry, dbConn });
 		console.log("Connected to database. Start sending student emails");
 		
-		// let studentIds = await db.getStudentIdsWhoWantDailyEmail();
-		let studentIds = ["101538478513395768684"];
+		// let studentIds = await db.getStudentIdsWhoWantDailyEmail(); // retrieve student ids
 
 		await sendEmails(db, studentIds); // send emails to retrieved student ids
 
