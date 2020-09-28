1a045b132eb5a1e8bcbfe1863047f31cf0b76ce0
zane-programs <zstjohn22@windwardschool.org>
2020-09-27 23:25:47 -0700

    update config for 4 day week

--- web-app/public-config.js
+++ web-app/public-config.js
@@ -1,4 +1,5 @@
 // I made this a JS file instead of JSON for ease of use.
 module.exports = {
-	schedule_folder_name: "schedules_2020_2021"
+	// schedule_folder_name: "schedules_2020_2021"
+	schedule_folder_name: "schedules_2020_2021_4Day_MonHoliday"
 };
