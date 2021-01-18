67f6205e5d3c59cc382067d38a4a56d20434df91
zane-programs <zstjohn22@windwardschool.org>
2021-01-18 01:56:08 -0800

    update schedule for MLK week

--- web-app/public-config.js
+++ web-app/public-config.js
@@ -1,7 +1,8 @@
 // I made this a JS file instead of JSON for ease of use.
 module.exports = {
-	schedule_folder_name: "schedules_2020_2021"
+	// schedule_folder_name: "schedules_2020_2021"
 	// schedule_folder_name: "schedules_2020_2021_4Day_MonHoliday"
 	// schedule_folder_name: "schedules_2020_2021_4Day_FriHoliday"
 	// schedule_folder_name: "schedules_2020_PreThanksgiving_Conferences_Week"
+	schedule_folder_name: "schedules_2020_2021_4Day_MonHoliday_MLK_Inauguration"
 };
