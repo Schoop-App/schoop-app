eb8fecde7865f7d00d2b0b825cb4cfebf4b25cb8
zane-programs <zstjohn22@windwardschool.org>
2021-04-12 00:08:55 -0700

    back to school

--- web-app/public-config.js
+++ web-app/public-config.js
@@ -1,5 +1,6 @@
 // I made this a JS file instead of JSON for ease of use.
 module.exports = {
 	// schedule_folder_name: "schedules_2020_2021"
-	schedule_folder_name: "schedules_2020_2021_March22_TransitionWeek_v2"
+	// schedule_folder_name: "schedules_2020_2021_March22_TransitionWeek_v2"
+	schedule_folder_name: "schedules_2020_2021_4Day_FriHoliday"
 };
