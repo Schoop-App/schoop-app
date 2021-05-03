3f233ab93898789f5561f70abb5ce2fd4cf6e396
zane-programs <zstjohn22@windwardschool.org>
2021-05-02 22:23:41 -0700

    update schedule for week of may 3 2021

--- web-app/public-config.js
+++ web-app/public-config.js
@@ -1,6 +1,5 @@
 // I made this a JS file instead of JSON for ease of use.
 module.exports = {
 	// schedule_folder_name: "schedules_2020_2021"
-	schedule_folder_name: "schedules_2020_2021_4Day_MonHoliday_Apr27_2021"
-	// schedule_folder_name: "schedules_2020_2021_4Day_FriHoliday"
+	schedule_folder_name: "schedules_2020_2021_May3_2021"
 };
