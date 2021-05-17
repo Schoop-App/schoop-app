c41e9cd88fd2d076974b5d30044266bf886a4ea1
zane-programs <zstjohn22@windwardschool.org>
2021-05-17 01:07:48 -0700

    fix schedule name

--- web-app/public-config.js
+++ web-app/public-config.js
@@ -1,5 +1,5 @@
 // I made this a JS file instead of JSON for ease of use.
 module.exports = {
 	// schedule_folder_name: "schedules_2020_2021"
-	schedule_folder_name: "schedules_2020_2021_May10_2021"
+	schedule_folder_name: "schedules_2020_2021_May17_2021"
 };
