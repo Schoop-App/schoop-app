83739200873b055398c86eed5087319239577067
zane-programs <zstjohn22@windwardschool.org>
2021-05-22 21:10:44 -0700

    Just a little bit of reorganization

--- web-app/app/core/schedules.js
+++ web-app/app/core/schedules.js
@@ -7,7 +7,7 @@ const SCHEDULE_TEMPLATE_DAYS = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
 
 const getSchedule = async (division, day) => {
 	if ((division === Division.MIDDLE || division === Division.UPPER) && SCHEDULE_TEMPLATE_DAYS.includes(day)) {
-		let scheduleFile = await readFileAsync(`${__dirname}/../../../${PUBLIC_CONFIG.schedule_folder_name || "schedules"}/${division}/${day}.json`);
+		let scheduleFile = await readFileAsync(`${__dirname}/../../../schedules/${PUBLIC_CONFIG.schedule_folder_name || "schedules"}/${division}/${day}.json`);
 		return JSON.parse(scheduleFile.toString());
 	} else {
 		throw new Error("Invalid Request – malformed schedule template query");
