c9a6ea9566e6648cb9f35f114ec18bfb41338e79
zane-programs <zstjohn22@windwardschool.org>
2021-08-30 00:04:01 -0700

    schedules for this week + a small addition to debug dialog

--- web-app/app/core/schedules.js
+++ web-app/app/core/schedules.js
@@ -6,12 +6,18 @@ const PUBLIC_CONFIG = require("../../public-config");
 const SCHEDULE_TEMPLATE_DAYS = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
 const getSchedule = async (division, day) => {
-	if ((division === Division.MIDDLE || division === Division.UPPER) && SCHEDULE_TEMPLATE_DAYS.includes(day)) {
-		let scheduleFile = await readFileAsync(`${__dirname}/../../../schedules/${PUBLIC_CONFIG.schedule_folder_name || "schedules"}/${division}/${day}.json`);
-		return JSON.parse(scheduleFile.toString());
-	} else {
+	// make sure the getSchedule call is valid
+	if ((division !== Division.MIDDLE && division !== Division.UPPER) || !SCHEDULE_TEMPLATE_DAYS.includes(day))
 		throw new Error("Invalid Request – malformed schedule template query");
+
+	// for weekends
+	if (day === "SAT" || day === "SUN") {
+		return { "message": "You have no classes today. Please check back later." };
 	}
+
+	// all other days
+	let scheduleFile = await readFileAsync(`${__dirname}/../../../schedules/${PUBLIC_CONFIG.schedule_folder_name || "schedules"}/${division}/${day}.json`);
+	return JSON.parse(scheduleFile.toString());
 };
 
 module.exports = {
