00c78e85d66ea0c08a957fc6e8696eb17c62b97d
zane-programs <zstjohn22@windwardschool.org>
2021-11-14 23:42:01 -0800

    update config

--- web-app/public-config.js
+++ web-app/public-config.js
@@ -1,4 +1,4 @@
 // I made this a JS file instead of JSON for ease of use.
 module.exports = {
-	schedule_folder_name: "schedules_2021_2022_Nov9_Special"
+	schedule_folder_name: "schedules_2021_2022_PGT_Conf_Nov_2021"
 };
