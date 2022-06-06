79b9ae495c200d934f508d951cf86e24e2bc59d4
Zane St. John <zstjohn@stanford.edu>
2022-06-06 01:10:21 -0700

    fix schedule setting

--- web-app/public-config.js
+++ web-app/public-config.js
@@ -1,5 +1,5 @@
 // I made this a JS file instead of JSON for ease of use.
 module.exports = {
   // schedule_folder_name: "schedules_2021_2022_Regular_Schedule",
-  schedule_folder_name: "schedules_2021_2022_May30_2022",
+  schedule_folder_name: "schedules_2021_2022_FinalsWeek",
 };
